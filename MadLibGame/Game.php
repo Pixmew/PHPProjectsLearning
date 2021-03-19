e01d68e6a43cfdbc8668cad4a722a448c27abce8
yash <universeyash4@gmail.com>
2021-03-19 01:48:43 -0700

    Added new project template to file called MadLibGame

---
+++ MadLibGame/Game.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>MadLibGame</title>
+  </head>
+  <body>
+    <form class="StoryInitilasization" action="Game.php" method="get">
+        <input type="text" name="" value="">
+    </form>
+  </body>
+</html>
