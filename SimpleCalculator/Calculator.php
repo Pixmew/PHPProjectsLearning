a9c9ace16ecb98a35295ca0da6eefd7d03aebcbd
Pixmew <>
2021-04-12 12:34:42 +0530

    modified name of numeriacal field to use array to astore numbes adn modified if statements to check for its accociated checkbox for operateon and not depend on other checkboxes

--- SimpleCalculator/Calculator.php
+++ SimpleCalculator/Calculator.php
@@ -5,9 +5,9 @@
     <title></title>
   </head>
   <body>
-    <form class="CalculatorInput" action="Calculator.php" method="GET">
-        Number 1 :<input type="number" name="number1">
-        Number 2 :<input type="number" name="number2"><br>
+    <form class="CalculatorInput" action="Calculator.php" method="POST">
+        Number 1 :<input type="number" name="number[]" value=0>
+        Number 2 :<input type="number" name="number[]" value=0><br>
         Addition :<input type="checkbox" name="addition">
         Substraction :<input type="checkbox" name="substraction">
         Multiplication :<input type="checkbox" name="multiplication">
@@ -17,18 +17,18 @@
     </form>
     <br>
         <?php
-
-            if($_GET["addition"] and !$_GET["substraction"] and !$_GET["multiplication"] and !$_GET["division"]){
-                echo ($_GET["number1"] + $_GET["number2"]);
+            $numbers = $_POST["number"];
+            if($_POST["addition"]){
+                echo ($numbers[0] + $numbers[1]);
             }
-            else if(!$_GET["addition"] and $_GET["substraction"] and !$_GET["multiplication"] and !$_GET["division"]){
-                echo ($_GET["number1"] - $_GET["number2"]);
+            else if($_POST["substraction"]){
+                echo ($numbers[0] - $numbers[1]);
             }
-            else if(!$_GET["addition"] and !$_GET["substraction"] and $_GET["multiplication"] and !$_GET["division"]){
-                echo ($_GET["number1"] * $_GET["number2"]);
+            else if($_POST["multiplication"]){
+                echo ($numbers[0] * $numbers[1]);
             }
-            else if(!$_GET["addition"] and !$_GET["substraction"] and !$_GET["multiplication"] and $_GET["division"]){
-                echo ($_GET["number1"] / $_GET["number2"]);
+            else if($_POST["division"]){
+                echo ($numbers[0] / $numbers[1]);
             }
         ?>
   </body>
