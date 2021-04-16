70c0da4ee973b550ccb1297659d6c425d2c030ba
Pixmew <>
2021-04-16 17:33:12 +0530

    Updated Calculatior.php to be more userfriendl

--- SimpleCalculator/Calculator.php
+++ SimpleCalculator/Calculator.php
@@ -8,10 +8,10 @@
     <form class="CalculatorInput" action="Calculator.php" method="POST">
         Number 1 :<input type="number" name="number[]" value=0>
         Number 2 :<input type="number" name="number[]" value=0><br>
-        Addition :<input type="checkbox" name="addition">
-        Substraction :<input type="checkbox" name="substraction">
-        Multiplication :<input type="checkbox" name="multiplication">
-        Division :<input type="checkbox" name="division">
+        Addition -<input type="checkbox" name="addition"><br>
+        Substraction -<input type="checkbox" name="substraction"><br>
+        Multiplication -<input type="checkbox" name="multiplication"><br>
+        Division -<input type="checkbox" name="division"><br>
         <br>
         <input type="submit" name="submit" >
     </form>
@@ -20,16 +20,17 @@
             error_reporting(0);
             $numbers = $_POST["number"];
             if($_POST["addition"]){
-                echo ($numbers[0] + $numbers[1]);
+
+                echo "Addition is -> ".($numbers[0] + $numbers[1]);
             }
-            else if($_POST["substraction"]){
-                echo ($numbers[0] - $numbers[1]);
+            if($_POST["substraction"]){
+                echo "Substration is -> ".($numbers[0] - $numbers[1]);
             }
-            else if($_POST["multiplication"]){
-                echo ($numbers[0] * $numbers[1]);
+            if($_POST["multiplication"]){
+                echo "Multiplication is -> ".($numbers[0] * $numbers[1]);
             }
-            else if($_POST["division"]){
-                echo ($numbers[0] / $numbers[1]);
+            if($_POST["division"]){
+              echo "Division is -> ".($numbers[0] / $numbers[1]);
             }
         ?>
   </body>
