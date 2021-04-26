5f5c03b3ba54a95078df5def8ada431d82d32a4a
Pixmew <>
2021-04-26 15:18:07 +0530

    added operatoes basics with few basic operations like assignement logial and or etc

---
+++ PHPLearningBasics/OperatorsBasics.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+      $firstNumber = 0;
+      $secondNumber = 0;
+
+      echo "Original First Number is :".$firstNumber;
+      echo "Original Second Number is :".$secondNumber;
+      echo "<br><br>";
+      echo "First number Increment post:".++$firstNumber."<br>";
+      echo "First number Decrement post:".--$firstNumber."<br>";
+      echo "First number Increment post:".$firstNumber++."<br>";
+      echo "First number Decrement post:".$firstNumber--."<br>";
+
+      if($firstNumber == $secondNumber){
+        echo "Equal Numbers";
+      }
+      else{
+        echo "Not Equal Numbers";
+      }
+
+      if(!$firstNumber > $secondNumber && !$firstNumber < $secondNumber){
+        echo "equal";
+      }
+      if(!($firstNumber > $secondNumber) and !($firstNumber < $secondNumber)){
+        echo "equal";
+      }
+      if(!$firstNumber > $secondNumber || !$firstNumber < $secondNumber){
+        echo "equal";
+      }
+      if(!$firstNumber > $secondNumber or !$firstNumber < $secondNumber){
+        echo "equal";
+      }
+
+    ?>
+  </body>
+</html>
